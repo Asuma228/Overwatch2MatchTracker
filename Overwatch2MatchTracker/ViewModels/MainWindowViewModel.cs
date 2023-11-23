e58d212da51bce69171f00df002814b6142c664e
Asuma228 <nk12041997@gmail.com>
2023-11-23 17:37:35 +0900

    Реализация шаблона MVVM 1

---
+++ Overwatch2MatchTracker/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,20 @@
+﻿using MathCore.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Overwatch2MatchTracker.ViewModels
+{
+    class MainWindowViewModel :ViewModel
+    {
+        #region Title : string Заголовок
+
+        private string _Title = "INPUT";
+
+        public string Title { get => _Title; set => Set(ref _Title, value); }
+
+        #endregion
+    }
+}
